963cf759a6d8208a5eeb67683c515f80ade70662
izyver <mr.izyver@gmail.com>
2020-12-14 19:51:14 +0200

    init commit

---
+++ app/src/main/java/com/sonerim/derect_wifi_test/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sonerim.derect_wifi_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
